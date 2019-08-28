532de4fd42dd6d045ede74ca5661cc748bf23607
zau19 <54337274+zau19@users.noreply.github.com>
2019-08-28 19:05:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var number = prompt ('what is your number?');
+if (number == 20 ){
+    console.log('You got it right');
+}
+else if (number > 20){
+    console.log('Too high. Guess agian!');
+} else {
+    console.log('Too low. Guess again!');
+}
